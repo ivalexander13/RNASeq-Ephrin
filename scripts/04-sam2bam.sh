6b965752b29778b98f1d61c242bd1026cae877d0
Ivan <ivalexander13@berkeley.edu>
2019-05-27 16:56:31 -0700

    Fixed bug where header of bam files are missing, by adding the parameter -h.

--- scripts/04-sam2bam.sh
+++ scripts/04-sam2bam.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 #
 # Announce start
-echo "# Running sam2bam.sh"
+echo "# Running sam2bam.sh" #
 # Importing directories.
 source init_dir.sh #
 #
-mkdir ./data/seqmonk_files #
-dir_seqmonk_files=./data/seqmonk_files #
+mkdir ./data/aligned_bams #
+dir_bams=./data/aligned_bams #
+dir_sams=./data/aligned_sams #
 #
-#for f in `find ${dir_fastqs}/*fastq | sed 's#'$dir_fastqs'/##' | sed 's#.fastq##' ` #
-#do #
-#echo "# Working on file ${f}..."
-${seqmonk_import} --genome "Rattus norvegicus/rn6" --outfile ${dir_seqmonk_files}/${f}_seqmonk.smk ${dir_sams}/*.sam #
-#done #
+for f in `find ${dir_sams}/*sam | sed 's#'$dir_sams'/##' | sed 's#.sam##' ` #
+do #
+echo "# Working on file ${f}..." #
+samtools view -h ${dir_sams}/${f}.sam > ${dir_bams}/${f}.bam #
+done #
 #
-# Bug [fixed]: last file does not work properly! I included the csv so f2 will work fine.
+#
+## Prompt to confirm whether or not the sam files are to be deleted.
+#read -p "Delete .sam files?" -n 1 -r #
+#echo    # (optional) move to a new line
+#if [[ $REPLY =~ ^[Yy]$ ]] #
+#then #
+#    rm -r ./data/aligned_sams #
+#fi #
+#
+#
+'''
 # Announce the end
 echo "# Finished running sam2bam.sh"
