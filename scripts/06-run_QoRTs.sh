69e82f164acb08b4e49681dd591bc0785bdacbda
Ivan <ivalexander13@berkeley.edu>
2019-05-30 00:50:49 -0700

    Added commands (v1).

--- scripts/06-run_QoRTs.sh
+++ scripts/06-run_QoRTs.sh
@@ -6,14 +6,15 @@ echo "# Running run_QoRTs.sh"
 source init_dir.sh #
 #
 # Create directory for alignment files.
+dir_sorted=./data/sorted_bams #
 mkdir ./results/QoRTs_results #
-dir_out = ./results/QoRTs_results#
+dir_out=./results/QoRTs_results #
 #
 # Loops through the fastq files and runs hisat2 on each one.
-for f in `find ${dir_fastqs}/*fastq | sed 's#'$dir_fastqs'/##' | sed 's#.fastq##' ` #
-do #
-echo "# Working on file ${f}..."
-hisat2 -q -t -x ${dir_indexes}/genome -U ${dir_fastqs}/${f}.fastq -S ${dir_sams}/${f}_aligned.sam #
-done #
+for f in `find ${dir_sorted}/*bam | sed 's#'${dir_sorted}'/##' | sed 's#.bam##' ` #
+  do #
+    echo "# Working on file ${f}..."
+    java.exe -Xmx2G -jar ./tools/QoRTs-1.3.6/QoRTs.jar QC --singleEnded --minMAPQ 40 --generatePlots --summaryFileSuffix .summary.txt --verbose ${dir_sorted}/${f}.bam ${f_annotation} ${dir_out}/${f}/ #
+  done #
 # Announce the end
 echo "# Finished running."
