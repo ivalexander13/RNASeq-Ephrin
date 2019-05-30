a0dd6956b02bf4e5b598e88ff7139fa9793a29cc
Ivan <ivalexander13@berkeley.edu>
2019-05-29 23:46:23 -0700

    Minor formatting changes.

--- scripts/03-align.sh
+++ scripts/03-align.sh
@@ -11,10 +11,10 @@ dir_sams=./data/aligned_sams #
 #
 # Loops through the fastq files and runs hisat2 on each one.
 for f in `find ${dir_fastqs}/*fastq | sed 's#'$dir_fastqs'/##' | sed 's#.fastq##' ` #
-do #
-echo "# Working on file ${f}..."
-hisat2 -q -t -x ${dir_indexes}/genome -U ${dir_fastqs}/${f}.fastq -S ${dir_sams}/${f}_aligned.sam #
-done #
+  do #
+    echo "# Working on file ${f}..."
+    hisat2 -q -t -x ${dir_indexes}/genome -U ${dir_fastqs}/${f}.fastq -S ${dir_sams}/${f}_aligned.sam #
+  done #
 # Bug [fixed]: last file does not work properly! I included the csv so f2 will work fine.
 # Announce the end
 echo "# Finished running align.sh"
