d2525c7ec32689b40285379e2c8d2a30aad36ab3
Ivan <ivalexander13@berkeley.edu>
2019-06-02 20:23:49 -0700

    Edited to work with our files.

--- results/QoRTs_results/a1_aligned_sorted/QC.makeMultiplot.R
+++ results/QoRTs_results/a1_aligned_sorted/QC.makeMultiplot.R
@@ -2,13 +2,13 @@
 message("STARTING...");
 library(QoRTs);
 unique.ID <- c("Untitled");
-qc.data.dir <- c("./results/QoRTs_results/a1_aligned_sorted//");
+qc.data.dir <- c("mnt/e/GDrive/cal-extras/schaffer_lab/RNASeq-Ephrin/results/QoRTs_results/a1_aligned_sorted/");
 qc.data.prefix <- c("");
 decoder.raw <- data.frame(unique.ID = as.character(unique.ID), qc.data.dir = as.character(qc.data.dir), qc.data.prefix=as.character(qc.data.prefix),stringsAsFactors=FALSE);
 decoder <- completeAndCheckDecoder(decoder = decoder.raw)
 message(decoder);
 message(lapply(names(decoder), function(n){ class(decoder[[n]]) }));
-res <- read.qc.results.data("", decoder = decoder, calc.DESeq2 = FALSE, calc.edgeR = FALSE);
+res <- read.qc.results.data("", decoder = decoder, calc.DESeq2 = FALSE, calc.edgeR = FALSE, autodetectMissingSamples = TRUE);
 makeMultiPlot.basic(res, outfile = "./results/QoRTs_results/a1_aligned_sorted//QC.multiPlot.png", plotter.params = list(std.color = "blue", std.lines.lwd = 4), plot.device.name = "png");
 makeMultiPlot.basic(res, outfile = "./results/QoRTs_results/a1_aligned_sorted//QC.multiPlot.pdf", plotter.params = list(std.color = "blue", std.lines.lwd = 4), plot.device.name = "pdf");
 makeMultiPlot.basic(res, outfile.dir = paste0("./results/QoRTs_results/a1_aligned_sorted/","QCplots/"), plotter.params = list(std.color = "blue", std.lines.lwd = 4), plot.device.name = "png", separatePlots = TRUE);
